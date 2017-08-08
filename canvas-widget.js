c013bfe0001147c58b9810b77324b729c7c36703
Jeroen van der Steen <info@jeroensteen.nl>
2017-08-08 20:17:48 +0200

    Added comments to Widget

--- canvas-widget.js
+++ canvas-widget.js
@@ -3,4 +3,11 @@ document.addEventListener('DOMContentLoaded', function () {
 	//Get all Canvas widgets
 	var cws = document.getElementsByClassName("ksart-canvas-widget");
 	console.log(cws);
+
+	//Get data-username attribute from Canvas widget Div
+
+	//Get talents data from user, Ajax
+
+	//Output data as HTML/CSS in builded Wiget
+
 });
