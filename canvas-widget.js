6e4ec3ca3c8000a6d40295e0ca42fe79493b6541
Jeroen van der Steen <info@jeroensteen.nl>
2017-08-08 20:05:34 +0200

    Reformated Widget

--- canvas-widget.js
+++ canvas-widget.js
@@ -1,6 +1,6 @@
 //Wait for Document is ready
 document.addEventListener('DOMContentLoaded', function () {
 	//Get all Canvas widgets
-    var cws = document.getElementsByClassName("ksart-canvas-widget");
+	var cws = document.getElementsByClassName("ksart-canvas-widget");
 	console.log(cws);
 });
